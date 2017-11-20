3c6ef8d2239172cfb2cf1600ba02d5a65a85dc04
jernejaMislej <jerneja.mislej@gmail.com>
2017-11-20 13:06:23 +0100

    bug: fixing upload_video and its args
    adjusting the arguments for the script

--- python/upload_video_with_preprocessing.py
+++ python/upload_video_with_preprocessing.py
@@ -2,6 +2,7 @@
 
 import os
 import argparse
+import random
 
 def run(cmd):
     os.system(" ".join(cmd))
@@ -16,8 +17,13 @@ def get_args():
     p.add_argument("--skip_sampling", help="Skip video sampling step", action="store_true")
     p.add_argument("--skip_upload", help="Skip upload images", action="store_true")
     p.add_argument("--user", help="User name")
+    p.add_argument("--userkey", help="User key")
     p.add_argument("--email", help="User email")
-    p.add_argument("--project", help="Specify project for the video")
+    p.add_argument("--project", help="Specify project for the video", default=None)
+    p.add_argument("--project_key", help="Specify project key for the video", default=None)
+    p.add_argument("--video_start_time", help="Specify the video start time",default=None)
+    p.add_argument('--offset_angle', default=0., type=float, help='offset camera angle (90 for right facing, 180 for rear facing, -90 for left facing)')
+    p.add_argument("--image_prefix", help="Specify the prefix for the images, to avoid same names", default=str(int(random.random()*1000000)))
 
     return p.parse_args()
 
@@ -25,28 +31,42 @@ if __name__ == "__main__":
 
     args = get_args()
 
-    image_path = os.path.basename(args.video_file).split(".")[0]
-
-    if os.path.exists(os.path.join(image_path, "PROCESSING_LOG.json")) is False:
+    if os.path.exists(os.path.join(args.image_path, "PROCESSING_LOG.json")) is False:
         cmd = ["python", "geotag_video.py",
                args.video_file,
                "--gps_trace", args.gps_trace,
-               "--image_path", image_path,
+               "--image_path", args.image_path,
                "--sample_interval", str(args.sample_interval)]
+        if args.video_start_time:
+            cmd.extend(["--video_start_time", str(args.video_start_time)])
+        if args.offset_angle:
+            cmd.extend(["--offset_angle", str(args.offset_angle)])
+        if args.image_prefix:
+            cmd.extend(["--image_prefix", args.image_prefix])
+
         run(cmd)
 
-    assert(args.user is not None and args.email is not None)
+    assert(args.user is not None and (args.email is not None or args.userkey is not None))
 
     if not args.skip_upload:
         upload_cmd = [
             "python", "upload_with_preprocessing.py",
-            image_path,
+            args.image_path,
             "--remove_duplicates",
             "--interpolate_directions",
             "--duplicate_distance", "0.5",
             "--duplicate_angle", "360",
             "--user", args.user,
-            "--email", args.email,
-            "--project", repr(args.project)
         ]
+        if args.email:
+            upload_cmd.extend(["--email", args.email])
+        if args.project:
+            upload_cmd.extend(["--project", repr(args.project)])        
+        if args.project_key:
+            upload_cmd.extend(["--project_key", args.project_key])      
+        if args.offset_angle:
+            upload_cmd.extend(["--offset_angle", args.offset_angle])
+        if args.userkey:
+            upload_cmd.extend(["--userkey", args.userkey])      
+    
         run(upload_cmd)
